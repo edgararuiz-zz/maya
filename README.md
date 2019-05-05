0fb98856f0358ab88dd0976c4077ce58a39810d5
Edgar Ruiz <edgar@rstudio.com>
2019-05-05 11:21:16 -0500

    Adds README

---
+++ README.md
@@ -0,0 +1,28 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# maya
+
+<!-- badges: start -->
+
+[![CRAN
+status](https://www.r-pkg.org/badges/version/maya)](https://cran.r-project.org/package=maya)
+<!-- badges: end -->
+
+The goal of maya is to …
+
+## Installation
+
+You can install the released version of maya from
+[CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("maya")
+```
+
+And the development version from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("edgararuiz/maya")
+```
--- tests/testthat/test-calendar.R
+++ tests/testthat/test-calendar.R
@@ -13,12 +13,12 @@ test_that("Gregorian to mayan converts correctly", {
 })
 
 test_that("Special add_days cases", {
-  expect_equivalent(add_days(3114, 8, 11, TRUE, 1), c(3114, 8, 12, 1 ))
-  expect_equivalent(add_days(3114, 8, 11, TRUE, 30), c(3114, 9, 10, 1 ))
+  expect_equivalent(add_days(3114, 8, 11, TRUE, 1),  c(3114, 8, 12, 1))
+  expect_equivalent(add_days(3114, 8, 11, TRUE, 30), c(3114, 9, 10, 1))
   expect_error(add_days(2000, 1, 1, FALSE, -1000))
 })
 test_that("Special diff_day cases", {
-  expect_equal(diff_days(2014, 2, 5, FALSE, 2000, 2, 4, FALSE),  -5115)
-  expect_equal(diff_days(2014, 2, 5, FALSE, 2014, 3, 4, FALSE),  58)
+  expect_equal(diff_days(2014, 2, 5, FALSE, 2000, 2, 4, FALSE), -5115)
+  expect_equal(diff_days(2014, 2, 5, FALSE, 2014, 3, 4, FALSE), 58)
 })
 
