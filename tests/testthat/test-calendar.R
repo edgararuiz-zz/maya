dc8ee1f771799f82e036162c3e53ed18be67a4ac
Edgar Ruiz <edgar@rstudio.com>
2019-05-05 21:35:41 -0500

    Adds tests

--- tests/testthat/test-calendar.R
+++ tests/testthat/test-calendar.R
@@ -4,12 +4,18 @@ test_that("Mayan to gregorian converts correctly", {
   expect_equivalent(mayan_to_gregorian2( 0,  0, 0,  0,  0), c(3114,  8, 11, TRUE))
   expect_equivalent(mayan_to_gregorian2(13,  0, 0,  0,  0), c(2012, 12, 21, FALSE))
   expect_equivalent(mayan_to_gregorian2( 7, 16, 6, 16, 18), c(  32,  9,  1, TRUE))
+  expect_equal(mayan_to_gregorian("0.0.0.0.0"), "August 11, 3114 BCE")
+  expect_equal(mayan_to_gregorian("13.0.0.0.0"), "December 21, 2012 CE")
+  expect_equal(mayan_to_gregorian("0.0.0.0.0", "astronomical"), "-3113/8/11")
+  expect_equal(mayan_to_gregorian("0.0.0.0.0", "number"), -31140811)
 })
 
 test_that("Gregorian to mayan converts correctly", {
   expect_equivalent(gregorian_to_mayan2(3114,  8, 11, TRUE),  c( 0,  0, 0,  0,  0))
   expect_equivalent(gregorian_to_mayan2(2012, 12, 21, FALSE), c(13,  0, 0,  0,  0))
   expect_equivalent(gregorian_to_mayan2(  32,  9,  1, TRUE),  c( 7, 16, 6, 16, 18))
+  expect_equal(gregorian_to_mayan("August 11, 3114 BCE"), "0.0.0.0.0")
+  expect_equal(gregorian_to_mayan("December 21, 2012 CE"), "13.0.0.0.0")
 })
 
 test_that("Special add_days cases", {
